89990ff845e285da4479b48a3d81bb03387e73b8
Fran Varney <fvarney@progress.com>
2016-04-23 17:22:45 -0400

    Knex init

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://ydfgtecgtlffbr:LAxMWV9GpTH4yQPxndggmSxyoY@ec2-54-225-197-143.compute-1.amazonaws.com:5432/d8bt95ojetv6hu?ssl=true',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations'
+    }
+  },
+  staging: {
+    client: 'postgresql',
+    connection: 'postgres://user:pass@port:5432/databaseName?ssl=true',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: 'postgres://user:pass@port:5432/databaseName?ssl=true',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations'
+    }
+  }
+};
