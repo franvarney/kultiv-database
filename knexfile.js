e6ad9cafd1760734ed55381d8e3fbc30da8476b0
Fran Varney <fvarney@progress.com>
2016-07-08 20:20:55 -0400

    Update to use main directory and compress knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,34 @@
-module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: 'postgres://ydfgtecgtlffbr:LAxMWV9GpTH4yQPxndggmSxyoY@ec2-54-225-197-143.compute-1.amazonaws.com:5432/d8bt95ojetv6hu?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './migrations',
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    }
-  },
-  staging: {
-    client: 'postgresql',
-    connection: 'postgres://user:pass@port:5432/databaseName?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './migrations',
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    }
-  },
-  production: {
-    client: 'postgresql',
-    connection: 'postgres://user:pass@port:5432/databaseName?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './migrations',
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: './seeds'
-    }
+const Assert = require('assert')
+
+const POSTGRES_URI = process.env.POSTGRES_URI
+
+Assert.equal(typeof POSTGRES_URI, typeof 'string', `POSTGRES_URI required,
+  formatted like: postgres://user:pass@port:5432/databaseName?ssl=true`)
+
+let common = {
+  client: 'postgresql',
+  pool: {
+    min: 2,
+    max: 10
   }
+}
+
+let main = Object.assign({}, common, {
+  migrations: {
+    directory: './main/migrations',
+    tableName: 'main_knex_migrations'
+  },
+  seeds: {
+    directory: './main/seeds'
+  }
+})
+
+// can add more database configurations here...
+
+function assignConnection(database, connection) {
+  return Object.assign({}, database, { connection })
+}
+
+module.exports = {
+  'main': assignConnection(main, POSTGRES_URI)
 }
