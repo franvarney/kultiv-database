d8016b792678910777dd70de16d3d8623e1049c9
Fran Varney <fvarney@progress.com>
2016-07-27 18:48:00 -0400

    Hardcode one set of values

--- main/seeds/user_auth_keys.js
+++ main/seeds/user_auth_keys.js
@@ -23,8 +23,8 @@ exports.seed = function (knex, Promise) {
     }),
     knex(TABLE_NAME).insert({
       user_id: 3,
-      hawk_id: Uuid(),
-      hawk_key: Uuid()
+      hawk_id: 'b39e1e3d-8187-4087-a89f-e3886b67297a',
+      hawk_key: '43a12a1e-309f-4dd2-922f-102ba19b76e7'
     })
   )
 }
